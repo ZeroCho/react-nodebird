94826df60f5b52b0fa9ea8e6b26b8f6236e5c9f1
zerocho <zerohch0@gmail.com>
2019-06-01 18:40:44 +0900

    Fix: s3 promisify

--- prepare/lambda/index.js
+++ prepare/lambda/index.js
@@ -3,37 +3,27 @@ const sharp = require('sharp');
 
 const s3 = new AWS.S3();
 
-exports.handler = (event, context, callback) => {
+exports.handler = async (event, context, callback) => {
   const Bucket = event.Records[0].s3.bucket.name;
   const Key = event.Records[0].s3.object.key;
   const filename = Key.split('/')[Key.split('/').length - 1];
   const ext = Key.split('.')[Key.split('.').length - 1];
   console.log('Key', Key, filename, ext);
-  s3.getObject({ Bucket, Key }, async (err, data) => {
-    if (err) {
-      console.log(err);
-      return callback(err);
-    }
+  try {
+    const data = await s3.getObject({ Bucket, Key }).promise();
     console.log('getObject', data);
     console.log('sharp', sharp(data.Body).resize);
-    try {
-      const buffer = await sharp(data.Body).resize(800).toBuffer();
-      console.log('resized', `thumb/${filename}`, buffer.length);
-      s3.putObject({
-        Bucket,
-        Key: `thumb/${filename}`,
-        Body: buffer,
-      }, (err) => {
-        if (err) {
-          console.log(err);
-          return callback(err);
-        }
-        console.log('put done');
-        return callback(null, `thumb/${filename}`);
-      });
-    } catch (err) {
-      console.log(err);
-      return callback(err);
-    }
-  });
+    const buffer = await sharp(data.Body).resize(800).toBuffer();
+    console.log('resized', `thumb/${filename}`, buffer.length);
+    await s3.putObject({
+      Bucket,
+      Key: `thumb/${filename}`,
+      Body: buffer,
+    }).promise();
+    console.log('put done');
+    return callback(null, `thumb/${filename}`);
+  } catch (err) {
+    console.log(err);
+    return callback(err);
+  }
 };
