0997c3c8e1f0d6fbc9a19225e4e9d4c02ff187ef
zerocho <zerohch0@gmail.com>
2019-06-01 18:48:46 +0900

    Fix: sharp to gm

--- prepare/lambda/index.js
+++ prepare/lambda/index.js
@@ -1,5 +1,5 @@
 const AWS = require('aws-sdk');
-const sharp = require('sharp');
+const gm = require('gm').subClass({ imageMagick: true });
 
 const s3 = new AWS.S3();
 
@@ -12,18 +12,22 @@ exports.handler = async (event, context, callback) => {
   try {
     const data = await s3.getObject({ Bucket, Key }).promise();
     console.log('getObject', data);
-    console.log('sharp', sharp(data.Body).resize);
-    const buffer = await sharp(data.Body).resize(800).toBuffer();
-    console.log('resized', `thumb/${filename}`, buffer.length);
-    await s3.putObject({
-      Bucket,
-      Key: `thumb/${filename}`,
-      Body: buffer,
-    }).promise();
-    console.log('put done');
-    return callback(null, `thumb/${filename}`);
+    gm(data.Body).resize(800, 800, '^').quality(90).toBuffer(ext, async (err, buffer) => {
+      if (err) {
+        console.error(err);
+        return callback(err);
+      }
+      console.log('resized', `thumb/${filename}`, buffer.length);
+      await s3.putObject({
+        Bucket,
+        Key: `thumb/${filename}`,
+        Body: buffer,
+      }).promise();
+      console.log('put done');
+      return callback(null, `thumb/${filename}`);
+    });
   } catch (err) {
-    console.log(err);
+    console.error(err);
     return callback(err);
   }
 };
