85e8e0f0c0fc1c256a52c0451e99c04987c34b78
zerocho <zerohch0@gmail.com>
2019-06-01 19:51:49 +0900

    Fix: gm to sharp

--- prepare/lambda/index.js
+++ prepare/lambda/index.js
@@ -1,34 +1,34 @@
 const AWS = require('aws-sdk');
-const gm = require('gm').subClass({ imageMagick: true });
+const Sharp = require('sharp');
 
-const s3 = new AWS.S3();
-
+const S3 = new AWS.S3({ region: 'ap-northeast-2' });
+​​
 exports.handler = async (event, context, callback) => {
   const Bucket = event.Records[0].s3.bucket.name;
   const Key = event.Records[0].s3.object.key;
   const filename = Key.split('/')[Key.split('/').length - 1];
   const ext = Key.split('.')[Key.split('.').length - 1];
-  console.log('Key', Key, filename, ext);
+​
+  const requiredFormat = ext === 'jpg' ? 'jpeg' : ext;// sharp에서는 jpg 대신 jpeg사용합니다
   try {
-    const data = await s3.getObject({ Bucket, Key }).promise();
-    console.log('getObject', data);
-    gm(Buffer.from(data.Body)).resize(800, 800).quality(90)
-      .toBuffer(ext === 'jpg' ? 'jpeg' : ext, async (err, buffer) => {
-        if (err) {
-          console.error(err);
-          return callback(err);
-        }
-        console.log('resized', `thumb/${filename}`, buffer.length);
-        await s3.putObject({
-          Bucket,
-          Key: `thumb/${filename}`,
-          Body: buffer,
-        }).promise();
-        console.log('put done');
-        return callback(null, `thumb/${filename}`);
-      });
-  } catch (err) {
-    console.error(err);
-    return callback(err);
+    const s3Object = await S3.getObject({ // S3에서 이미지를 받아 옵니다.
+      Bucket: Bucket,
+      Key: filename,
+    }).promise();
+​
+    const resizedImage = await Sharp(s3Object.Body)
+      .resize(800, 800)
+      .toFormat(requiredFormat)
+      .toBuffer();
+​
+    await S3.putObject({
+      Body: resizedImage,
+      Bucket,
+      Key: `thumb/${filename}`,
+    });
+    return callback(null, `thumb/${filename}`);
+  } catch (error) {
+    console.error(error);
+    return callback(error);
   }
 };
