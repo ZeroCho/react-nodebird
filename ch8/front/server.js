73630a9c8270f44cd0096b0aa36777560343ae90
zerocho <zerohch0@gmail.com>
2019-06-05 22:37:03 +0900

    Update: front server port

--- ch8/front/server.js
+++ ch8/front/server.js
@@ -47,7 +47,7 @@ app.prepare().then(() => {
     return handle(req, res);
   });
 
-  server.listen(3060, () => {
-    console.log('next+express running on port 3060');
+  server.listen(prod ? process.env.PORT : 3060, () => {
+    console.log(`next+express running on port ${process.env.PORT}`);
   });
 });
