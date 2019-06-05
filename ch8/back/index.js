6ecec7f1b84178ba95865a8dca072de3007bd2e8
zerocho <zerohch0@gmail.com>
2019-06-05 22:52:30 +0900

    Add: back index route

--- ch8/back/index.js
+++ ch8/back/index.js
@@ -40,6 +40,10 @@ app.use(expressSession({
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.get('/', (req, res) => {
+  res.send('react nodebird 백엔드 정상 동작!');
+});
+
 // API는 다른 서비스가 내 서비스의 기능을 실행할 수 있게 열어둔 창구
 app.use('/api/user', userAPIRouter);
 app.use('/api/post', postAPIRouter);
