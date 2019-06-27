d689cc885c8e8753867aed79159b8580ba83725f
Hyunyoung Cho <zerohch0@gmail.com>
2019-06-27 12:04:36 +0900

    Update user.js

--- ch8/front/reducers/user.js
+++ ch8/front/reducers/user.js
@@ -4,7 +4,7 @@ export const initialState = {
   isLoggingOut: false, // 로그아웃 시도중
   isLoggingIn: false, // 로그인 시도중
   logInErrorReason: '', // 로그인 실패 사유
-  signedUp: false, // 회원가입 성공
+  isSignedUp: false, // 회원가입 성공
   isSigningUp: false, // 회원가입 시도중
   signUpErrorReason: '', // 회원가입 실패 사유
   me: null, // 내 정보
@@ -64,18 +64,18 @@ export default (state = initialState, action) => {
   return produce(state, (draft) => {
     switch (action.type) {
       case LOG_IN_REQUEST: {
-        draft.isLoggedIn = true;
+        draft.isLoggingIn = true;
         draft.logInErrorReason = '';
         break;
       }
       case LOG_IN_SUCCESS: {
-        draft.isLoggedIn = true;
+        draft.isLoggingIn = false;
         draft.logInErrorReason = '';
         draft.me = action.data;
         break;
       }
       case LOG_IN_FAILURE: {
-        draft.isLoggedIn = false;
+        draft.isLoggingIn = false;
         draft.logInErrorReason = action.reason;
         draft.me = null;
         break;
