4482535d8deceb039985be79ca26863e46cc3334
ZeroCho <zerohch0@gmail.com>
2020-07-07 00:41:00 +0900

    Update: backend port

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -57,6 +57,6 @@ app.use('/post', postRouter);
 app.use('/user', userRouter);
 app.use('/hashtag', hashtagRouter);
 
-app.listen(3065, () => {
+app.listen(80, () => {
   console.log('서버 실행 중!');
 });
