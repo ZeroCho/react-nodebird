04906deba90c9d879bd774c163ad1a5832c754a5
ZeroCho <zerohch0@gmail.com>
2020-07-07 17:44:24 +0900

    Add: thumbnail

--- prepare/back/routes/post.js
+++ prepare/back/routes/post.js
@@ -82,7 +82,7 @@ router.post('/', isLoggedIn, upload.none(), async (req, res, next) => { // POST 
 
 router.post('/images', isLoggedIn, upload.array('image'), (req, res, next) => { // POST /post/images
   console.log(req.files);
-  res.json(req.files.map((v) => v.location));
+  res.json(req.files.map((v) => v.location.replace(/\/original\//, '/thumb/')));
 });
 
 router.get('/:postId', async (req, res, next) => { // GET /post/1
--- prepare/front/components/ImagesZoom/index.js
+++ prepare/front/components/ImagesZoom/index.js
@@ -26,7 +26,7 @@ const ImagesZoom = ({ images, onClose }) => {
           >
             {images.map((v) => (
               <ImgWrapper key={v.src}>
-                <img src={`${v.src}`} alt={v.src} />
+                <img src={`${v.src.replace(/\/thumb\//, '/original/')}`} alt={v.src} />
               </ImgWrapper>
             ))}
           </Slick>
