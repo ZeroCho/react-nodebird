7086e03750a4c1d88e8629e8d75a62b18121ad23
ZeroCho <zerohch0@gmail.com>
2020-07-07 16:49:29 +0900

    Fix: cors

--- prepare/back/app.js
+++ prepare/back/app.js
@@ -29,13 +29,17 @@ if (process.env.NODE_ENV === 'production') {
   app.use(morgan('combined'));
   app.use(hpp());
   app.use(helmet());
+  app.use(cors({
+    origin: 'http://nodebird.com',
+    credentials: true,
+  }));
 } else {
   app.use(morgan('dev'));
+  app.use(cors({
+    origin: true,
+    credentials: true,
+  }));
 }
-app.use(cors({
-  origin: ['http://localhost:3060', 'nodebird.com'],
-  credentials: true,
-}));
 app.use('/', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
