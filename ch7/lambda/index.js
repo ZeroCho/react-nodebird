5de2de1e522f5fbf2e3fab7504ff7762e68799cf
ZeroCho <zerohch0@gmail.com>
2020-10-03 14:04:39 +0900

    Fix: lambda korean and ext lowerCase

--- ch7/lambda/index.js
+++ ch7/lambda/index.js
@@ -7,8 +7,8 @@ exports.handler = async (event, context, callback) => {
   const Bucket = event.Records[0].s3.bucket.name; // react-nodebird-s3
   const Key = event.Records[0].s3.object.key; // original/12312312_abc.png
   console.log(Bucket, Key);
-  const filename = Key.split('/')[Key.split('/').length - 1];
-  const ext = Key.split('.')[Key.split('.').length - 1];
+  const filename = decodeURIComponent(Key.split('/')[Key.split('/').length - 1]);
+  const ext = Key.split('.')[Key.split('.').length - 1].toLowerCase();
   const requiredFormat = ext === 'jpg' ? 'jpeg' : ext;
   console.log('filename', filename, 'ext', ext);
 
