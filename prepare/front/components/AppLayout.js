17f3abeb65908fc3a19d6d3000ca41af548ecacc
NakSeoNo <nakseono@gmail.com>
2020-11-26 15:55:55 +0900

    link & eslint

--- prepare/front/components/AppLayout.js
+++ prepare/front/components/AppLayout.js
@@ -1,10 +1,21 @@
 import React from "react";
-import PropTypes from "prop-types";
+import Link from "next/link";
 
 const AppLayout = ({ children }) => {
   return (
     <div>
-      <div>공통메뉴</div>
+      <div>
+        <Link href="/">
+          <a>노드버드</a>
+        </Link>
+        <Link href="/profile">
+          <a>프로필</a>
+        </Link>
+
+        <Link href="/signup">
+          <a>회원가입</a>
+        </Link>
+      </div>
       {children}
     </div>
   );
