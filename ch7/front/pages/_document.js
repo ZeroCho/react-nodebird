b14a3760b274aa7501b00d0e317956fe3541eb70
zerocho <zerohch0@gmail.com>
2020-11-29 16:20:23 +0900

    Fix: document.js typo

--- ch7/front/pages/_document.js
+++ ch7/front/pages/_document.js
@@ -3,12 +3,12 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
-  static async getInitalProps(ctx) {
+  static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
     try {
       ctx.renderPage = () => originalRenderPage({
-        enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
+        enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
       });
       const initialProps = await Document.getInitialProps(ctx);
       return {
