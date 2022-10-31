c816470a35ba53e9c8374785482b199a7eaa6313
wonny2 <onehee2choi@gmail.com>
2022-10-31 17:12:23 +0900

    signup

--- prepare/front/pages/signup.js
+++ prepare/front/pages/signup.js
@@ -1,8 +1,9 @@
 import AppLayout from "../components/AppLayout";
 import React, { useCallback, useState } from "react";
 import Head from 'next/head'
-import {Form,Input,Checkbox} from 'antd'
+import {Form,Input,Checkbox, Button} from 'antd'
 import styled from "styled-components";
+import useInput from '../hooks/useInput'
 
 const ErrorMessage = styled.div`
     color:red;
@@ -12,7 +13,7 @@ const ErrorMessage = styled.div`
 
 const SignUp = () => {
 
-    const [id, onChangeId] = useInput('')
+    const [id, onChangeId] = useInput("")
     // 이 한 줄 자체가 
     // const [id, setId] = useState('')
     // const onChangeId = useCallback((e) => {
@@ -20,17 +21,33 @@ const SignUp = () => {
    // })
     //  요 두 기능과 같은 것이다..!!
 
-    const[nickname, onChangeNickname] = useInput('');
-    const[password, onChangePassword] = useInput('');
-    const [passwordCheck, setPasswordCheck] = useState('');
+    const[nickname, onChangeNickname] = useInput("");
+    const[password, onChangePassword] = useInput("");
+    const [passwordCheck, setPasswordCheck] = useState("");
     const[passwordError, setPasswordError] = useState(false)
     const [term, setTerm] = useState('');
     const[termError, setTermError] = useState(false);
 
     const onChangeTerm = useCallback((e) => {
-        setTerm(e.target,checked)
+        setTerm(e.target.checked)
         setTermError(false)
-    })
+    },[])
+
+
+    const onSubmit = useCallback(() => {
+        
+
+        if(password !== passwordCheck) {
+            return setPasswordError(true)
+        }
+
+        if(!term) {
+            return setTermError(true)
+        }
+
+        console.log(id, nickname, password)
+
+    },[password,passwordCheck,term])
 
    const onChangePasswordCheck = useCallback((e) => {
         setPasswordCheck(e.target.value)
@@ -72,11 +89,16 @@ const SignUp = () => {
                 <Input name="user-id" value={passwordCheck} onChange={onChangePasswordCheck} required /> 
                 {passwordError && <ErrorMessage>비밀번호가 일치하지 않습니다.</ErrorMessage>}
             </div>            
-        </Form>
-        <div>
+        
+            <div>
                 <Checkbox name="user-term" checked={term} onChange={onChangeTerm}>꾸준히 할 것을 동의합니다.</Checkbox> 
                 {termError && <ErrorMessage>약관에 동의하셔야 합니다.</ErrorMessage>}
+            </div>
+
+            <div style={{marginTop: 10}}>
+                <Button type="primary" htmlType="submit">가입하기</Button>    
             </div>  
+            </Form>
         </AppLayout>
     )
 }
